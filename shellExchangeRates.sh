3bcef9dd85d91e76f51aa19ed14f54faa2d971df
sichiiii <76742792+sichiiii@users.noreply.github.com>
2021-03-08 16:20:50 +0300

    Update shellExchangeRates.sh

--- shellExchangeRates.sh
+++ shellExchangeRates.sh
@@ -5,20 +5,19 @@ clear
 echo "Newest exchange rates"
 echo "..."
 
-sed -n 1393p /home/jabka/python/DiscoTimeBash/page.txt > modPage.txt  
-cut -c56-105 /home/jabka/python/DiscoTimeBash/modPage.txt
-sed -n 1402p /home/jabka/python/DiscoTimeBash/page.txt > modPage.txt
-cut -c56-105 /home/jabka/python/DiscoTimeBash/modPage.txt
-sed -n 1411p /home/jabka/python/DiscoTimeBash/page.txt > modPage.txt
-cut -c56-105 /home/jabka/python/DiscoTimeBash/modPage.txt
-sed -n 1420p /home/jabka/python/DiscoTimeBash/page.txt > modPage.txt
-cut -c56-105 /home/jabka/python/DiscoTimeBash/modPage.txt
-sed -n 1429p /home/jabka/python/DiscoTimeBash/page.txt > modPage.txt
-cut -c56-130 /home/jabka/python/DiscoTimeBash/modPage.txt
-sed -n 1438p /home/jabka/python/DiscoTimeBash/page.txt > modPage.txt
-cut -c56-130 /home/jabka/python/DiscoTimeBash/modPage.txt
+sed -n 1393p page.txt > modPage.txt  
+cut -c56-105 modPage.txt
+sed -n 1402p page.txt > modPage.txt
+cut -c56-105 modPage.txt
+sed -n 1411p page.txt > modPage.txt
+cut -c56-105 modPage.txt
+sed -n 1420p page.txt > modPage.txt
+cut -c56-105 modPage.txt
+sed -n 1429p page.txt > modPage.txt
+cut -c56-130 modPage.txt
+sed -n 1438p page.txt > modPage.txt
+cut -c56-130 modPage.txt
 
 echo '...'
 rm page.txt
 rm modPage.txt
-
